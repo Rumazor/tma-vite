07ffa1e54c5e3a9d8050d1e805dd7b68696b932b
Rumazor <th3rum2084@gmail.com>
2024-06-21 18:06:37 -0500

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,12 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>QUE PASO JEFE</h1>
+      <div className="card"></div>
       {/* Here we add our button with alert callback */}
       <div className="card">
         <button
@@ -31,7 +24,7 @@ function App() {
             WebApp.showAlert(`Hello World! Current count is ${count}`)
           }
         >
-          Show Alert
+          Pequeña no digas eso
         </button>
       </div>
     </>
